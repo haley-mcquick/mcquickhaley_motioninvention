1d8847b6f45702d1f5328867fb95f796ce0c3317
haley-mcquick <72761290+haley-mcquick@users.noreply.github.com>
2021-02-07 21:28:26 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ while True:
     y_axis = input.acceleration(Dimension.Y)
     z_axis = input.acceleration(Dimension.Z)
     print("X-Axis: " + x_axis + " Y-Axis: " + y_axis + " Z-Axis: " + z_axis)
+    print(input.light_level)
 
 
-    if x_axis > 3 or y_axis > 3 or z_axis > 3:
-        light.set_all(color.rgb(255, 0, 0))
-        music.siren.loop()
+    while input.light_level() > 5:
+        if x_axis > 3 or y_axis > 3 or z_axis > 3:
+            light.set_all(color.rgb(255, 0, 0))
+            music.siren.loop()
 
         
--- main.ts
+++ main.ts
@@ -6,9 +6,12 @@ while (true) {
     y_axis = input.acceleration(Dimension.Y)
     z_axis = input.acceleration(Dimension.Z)
     console.log("X-Axis: " + x_axis + " Y-Axis: " + y_axis + " Z-Axis: " + z_axis)
-    if (x_axis > 3 || y_axis > 3 || z_axis > 3) {
-        light.setAll(color.rgb(255, 0, 0))
-        music.siren.loop()
+    console.log(input.lightLevel)
+    while (input.lightLevel() > 5) {
+        if (x_axis > 3 || y_axis > 3 || z_axis > 3) {
+            light.setAll(color.rgb(255, 0, 0))
+            music.siren.loop()
+        }
+        
     }
-    
 }
